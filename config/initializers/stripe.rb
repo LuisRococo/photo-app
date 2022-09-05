627023f806b62ca42bbaf6dde5016209b79b9c98
Luis Roberto <luisrobertopenav@gmail.com>
2022-09-05 13:16:58 -0700

    Add stripe payment for users

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,3 @@
-p ENV['STRIPE_TEST_PUBLISHABLE_KEY']
-
 Rails.configuration.stripe = {
   :publishable_ke => Rails.application.credentials.stripe[:STRIPE_TEST_PUBLISHABLE_KEY],
   :secret_key => Rails.application.credentials.stripe[:STRIPE_TEST_SECRET_KEY]
