81cb78e1a1f5cf00b060b61dc226730e8311283b
Luis Roberto <luisrobertopenav@gmail.com>
2022-09-05 12:48:49 -0700

    Add stripe scripts

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,5 @@
+p ENV['STRIPE_TEST_PUBLISHABLE_KEY']
+
 Rails.configuration.stripe = {
   :publishable_ke => Rails.application.credentials.stripe[:STRIPE_TEST_PUBLISHABLE_KEY],
   :secret_key => Rails.application.credentials.stripe[:STRIPE_TEST_SECRET_KEY]
